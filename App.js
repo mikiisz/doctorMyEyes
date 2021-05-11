a4d87b5ad03768be97540fb8acdabf3ef22d52b9
moniad <moni.dziedzic@gmail.com>
2021-05-11 23:17:24 +0200

    Added initial letters view

--- App.js
+++ App.js
@@ -1,59 +1,63 @@
 import React from 'react';
-import {StatusBar} from "expo-status-bar";
-import {Text, View} from "react-native";
+import {StyleSheet, Text, View} from "react-native";
+import LetterListGenerator from './letter'
 
-class FirstRow extends React.Component {
+class Letter extends React.Component {
     constructor(props) {
         super(props);
+        this.letterSize = props.letterSize
+        this.character = props.character
+    }
+
+    // render() {
+    //     return (
+    //         <View>
+    //             <Text style={{fontSize: this.letterSize}}>{this.character}</Text>
+    //         </View>
+    //     )
+    // }
+}
+
+class Row extends React.Component {
+    constructor(props) {
+        super(props);
+        this.letterSize = props.letterSize
+        this.numberOfLetters = props.numberOfLetters
+        this.state = {
+            generatedLettersList: ''
+        }
     }
 
     componentDidMount() {
-        // this.timerID = setInterval(
-        //     () => this.tick(),
-        //     1000
-        // );
-    }
-
-    componentWillUnmount() {
-        // clearInterval(this.timerID);
+        this.setState({
+            generatedLettersList: new LetterListGenerator(this.numberOfLetters).generateLetters()
+        })
     }
 
     render() {
-        return <View>
-            <Text>Hello first</Text>
-            <StatusBar style="auto"/>
-        </View>;
-    }
-}
-
-class SecondRow extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return <View>
-            <Text>Hello second</Text>
-            <StatusBar style="auto"/>
-        </View>;
-    }
-}
-
-class ThirdRow extends React.Component {
-    render() {
-        return <View>
-            <Text>Hello third</Text>
-            <StatusBar style="auto"/>
-        </View>;
+        return (
+            <View>
+                <Text style={{fontSize: this.letterSize}}>A</Text>
+            </View>
+        );
     }
 }
 
 export default function App() {
     return (
-        <View>
-            <FirstRow/>
-            <SecondRow/>
-            <ThirdRow/>
+        <View style={styles.container}>
+            <Row letterSize={30} numberOfLetters={1}/>
+            <Row letterSize={20} numberOfLetters={1}/>
+            <Row letterSize={10} numberOfLetters={1}/>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#2c7587',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
