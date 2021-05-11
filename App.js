909fd8d50356e3dc7847eeb1fb33786f47d3ccfb
moniad <moni.dziedzic@gmail.com>
2021-05-11 21:18:29 +0200

    Added basic component classes

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from "expo-status-bar";
+import {Text, View} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class FirstRow extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        // this.timerID = setInterval(
+        //     () => this.tick(),
+        //     1000
+        // );
+    }
+
+    componentWillUnmount() {
+        // clearInterval(this.timerID);
+    }
+
+    render() {
+        return <View>
+            <Text>Hello first</Text>
+            <StatusBar style="auto"/>
+        </View>;
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class SecondRow extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <View>
+            <Text>Hello second</Text>
+            <StatusBar style="auto"/>
+        </View>;
+    }
+}
+
+class ThirdRow extends React.Component {
+    render() {
+        return <View>
+            <Text>Hello third</Text>
+            <StatusBar style="auto"/>
+        </View>;
+    }
+}
+
+export default function App() {
+    return (
+        <View>
+            <FirstRow/>
+            <SecondRow/>
+            <ThirdRow/>
+        </View>
+    );
+}
