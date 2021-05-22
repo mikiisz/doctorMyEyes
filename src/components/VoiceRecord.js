ad0b72ad2102605334b65132840843f0f19cef85
Michal Szkarlat <michal.szkarlat@grandparade.co.uk>
2021-05-22 18:01:38 +0200

    Send via buffer

--- src/components/VoiceRecord.js
+++ src/components/VoiceRecord.js
@@ -2,19 +2,16 @@ import React from 'react';
 import { Audio } from 'expo-av';
 import config from '../../config';
 import { View, Button } from 'react-native';
+import { Buffer } from "buffer";
 import * as FileSystem from 'expo-file-system';
 
 function VoiceRecord(props) {
     const [recording, setRecording] = React.useState(undefined);
     const [transcript, setTranscript] = React.useState(undefined);
 
+
     async function startRecording() {
         await Audio.requestPermissionsAsync();
-        await Audio.setAudioModeAsync({
-            allowsRecordingIOS: true,
-            playsInSilentModeIOS: true,
-        });
-
         await Audio.setAudioModeAsync({
             allowsRecordingIOS: true,
             interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
@@ -23,14 +20,22 @@ function VoiceRecord(props) {
             interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
             playThroughEarpieceAndroid: true,
         });
+
         const recording = new Audio.Recording();
 
         try {
-            await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+            const { ios, android } = Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+            await recording.prepareToRecordAsync({
+                android: android,
+                ios: {
+                    ...ios,
+                    extension: '.wav',
+                }
+
+            })
             await recording.startAsync();
         } catch (error) {
             console.error(error);
-            stopRecording();
         }
         setRecording(recording);
     }
@@ -38,19 +43,26 @@ function VoiceRecord(props) {
     async function getTranscription() {
         try {
             await recording.stopAndUnloadAsync();
-            const info = await FileSystem.getInfoAsync(recording.getURI());
-            console.log(`file info: ${JSON.stringify(info)}`);
-            const uri = info.uri;
+            const uri = recording.getURI()
+            console.log(uri);
             const formData = new FormData();
             formData.append('file', {
                 uri,
-                type: 'application/pdf',
-                name: 'speech2text'
+                type: 'audio/wav',
+                name: 'speech2text.wav'
             });
+
+            const base64 = await FileSystem.readAsStringAsync(uri, { encoding: 'base64' });
+            const buffer = new Buffer.from(base64, 'base64')
             const response = await fetch(config.TRANSCRIPT_GATEWAY, {
                 method: 'POST',
-                body: formData
-            });
+                headers: {
+                    'Content-Type': 'audio/wav; charset=utf-8',
+                    'x-amz-acl': 'public-read',
+                },
+                body: buffer,
+            })
+
             const data = await response.json();
             setTranscript(data);
             console.log(`response: ${JSON.stringify(data)}`);
